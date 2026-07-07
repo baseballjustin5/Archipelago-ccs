76f697b0bc81c5bc413126505adfa7e7d786badd
Justin <baseballjustin5@users.noreply.github.com>
2026-07-07 06:41:12 -0500

    Added Trap choice options to options.py in order for trap functionality (not implemented in game atm)

--- worlds/ccs/options.py
+++ worlds/ccs/options.py
@@ -1,6 +1,23 @@
 from dataclasses import dataclass
-from Options import Choice, PerGameCommonOptions
+from Options import Toggle, Range, OptionSet, PerGameCommonOptions
+
+class EnableTraps(Toggle):
+    display_name = "Enable Traps?"
+    default = False
+
+class TrapDensity(Range):
+    display_name = "Trap Desnity"
+    range_start = 0
+    range_end = 100
+    default = 10
+
+class Traps(OptionSet):
+    display_name = "Enabled Trap Types"
+    valid_keys = {"Trash Items", "Wet Bills", "Dirty Bills", "Inked Bills", "Goo Bills", "All"}
+    default = {"All"}
 
 @dataclass
 class CashCleanerSimulatorOptions(PerGameCommonOptions):
-    pass
+    enable_traps: EnableTraps
+    trap_density: TrapDensity
+    selected_traps: Traps
